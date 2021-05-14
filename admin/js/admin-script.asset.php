41b21a9bf6e6eeb97e365f3ef6904fdd13689ba5
Erik Yo <erikyo83@gmail.com>
2021-05-14 16:49:23 +0200

    admin scripts

---
+++ admin/js/admin-script.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '51048ab80827f1e393297253030f89fa');
---
+++ includes/js/script.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'c4bec9931bc707f37e195b377344894f');
