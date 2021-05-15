26e3e2a2353fc5fc7df036dea501d44eb9026f03
Erik Yo <erikyo83@gmail.com>
2021-05-15 16:13:27 +0200

    Updated plugin dashboard

--- admin/admin-display.php
+++ admin/admin-display.php
@@ -1,14 +1,28 @@
 <?php
+// this is the plugin configuration page
+?>
+<div class="wrap">
+	<div class="card">
+		<h1><span class="dashicons dashicons-shield-alt"></span>Contact Form 7 - AntiSpam</h1>
+		<div class="inside">
+			<form method="post" action="options.php" id="cf7a_settings">
+			<?php
+				// This prints out all hidden setting fields
+				settings_fields( 'cf7_antispam_options' );
+				do_settings_sections( 'cf7a-settings' ) ;
 
-echo '<div class="wrap"><div class="card"><h1><span class="dashicons dashicons-shield-alt"></span>Contact Form 7 - AntiSpam</h1>';
-echo '<div class="inside"><form method="post" action="options.php">';
+				submit_button();
+			?>
+			</form>
+		</div>
+	</div>
 
-// This prints out all hidden setting fields
-settings_fields( 'cf7_antispam_options' );
-do_settings_sections( 'cf7a-settings' ) ;
+<?php if (WP_DEBUG) {
+	echo '<div class="card">';
+	echo '<h3>'.__('Debug info').'</h3>';
+	echo '<p>'.__('If you see this box it is because wp_debug is active!').'</p>';
 
-submit_button();
-echo '</form></div></div></div>';
-
-print_r("options<br/>");
-var_dump(get_option( 'cf7a_options' ));
+	echo '<pre>' . htmlentities(print_r(get_option( 'cf7a_options' ), true)) . '</pre>';
+	echo '</div>';
+}
+echo '</div>';
