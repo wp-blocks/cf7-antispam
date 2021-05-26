8dc2400b784615207fbce37084ac1b1626d5038c
Erik Yo <erikyo83@gmail.com>
2021-05-26 13:14:48 +0200

    updated style

--- admin/css/style.asset.php
+++ admin/css/style.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '8c066c307cd52d2644b8165d39486d28');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'd8d6c71691588d466d75c057a3304701');
