ccb4dd463b8bf70f1537acd28cebfba5f55b5ce1
Erik Yo <erikyo83@gmail.com>
2021-05-26 13:11:02 +0200

    ctrl+s to save settings

--- admin/src/admin-script.js
+++ admin/src/admin-script.js
@@ -1,14 +1,4 @@
-( function( $ ) {
-  // save on ctrl-s keypress
-  if (document.body.classList.contains('contact_page_cf7-antispam')) {
-    document.addEventListener('keydown', e => {
-      if (e.ctrlKey && e.key === 's') {
-        e.preventDefault();
-        document.getElementById('submit').click();
-      }
-    });
-  }
-
+let jquery = ( function($) {
   $( function() {
     var welcomePanel = $('#welcome-panel');
 
@@ -18,3 +8,13 @@
     });
   });
 });
+
+// save on ctrl-s keypress
+if (document.body.classList.contains('cf7-antispam-admin')) {
+  document.addEventListener('keydown', e => {
+    if (e.ctrlKey && e.key === 's') {
+      e.preventDefault();
+      document.getElementById('submit').click();
+    }
+  });
+}
