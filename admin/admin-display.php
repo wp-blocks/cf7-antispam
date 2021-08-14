246a68bb8ba9151fde8ef1d0951e1571fe68a1c9
Erik Yo <erikyo83@gmail.com>
2021-08-14 14:48:18 +0200

    I hope I have made the text of the banner message clearer.

--- admin/admin-display.php
+++ admin/admin-display.php
@@ -46,20 +46,28 @@ class CF7_AntiSpam_Admin_Display {
 	  $dismissible_banner_class = ( get_user_meta( get_current_user_id(), 'cf7a_hide_welcome_panel_on', true ) ) ? ' hidden' : '';
     ?>
 
-    <div class="card welcome-panel banner dismissible<?php echo $dismissible_banner_class ?>"><div class="inside">
-      <a class="welcome-panel-close" href="<?php echo esc_url( add_query_arg( 'action', 'dismiss-banner', menu_page_url( 'cf7-antispam', false ) ) ); ?>"><?php echo esc_html( __( 'Dismiss', 'contact-form-7' ) ); ?></a>
-      <h3><span class="dashicons dashicons-editor-help" aria-hidden="true"></span> <?php echo esc_html( __( "Before you cry over spilt mail&#8230;", 'contact-form-7' ) ); ?></h3>
-      <p><?php echo esc_html( __( "Contact Form 7 doesn&#8217;t store submitted messages anywhere. Therefore, you may lose important messages forever if your mail server has issues or you make a mistake in mail configuration.", 'contact-form-7' ) ); ?></p>
-      <p><?php echo sprintf( /* translators: %s: link labeled 'Flamingo' */
-			  esc_html( __( 'Install a message storage plugin before this happens to you. %s saves all messages through contact forms into the database. Flamingo is a free WordPress plugin created by the same author as Contact Form 7.', 'contact-form-7' ) ), wpcf7_link( __( 'https://contactform7.com/save-submitted-messages-with-flamingo/', 'contact-form-7' ), __( 'Flamingo', 'contact-form-7' ) ) );
-		  ?></p>
-
-        <hr />
-
-      <h3 class="blink"><span class="dashicons dashicons-megaphone" aria-hidden="true"></span> <?php echo esc_html( __( "And PLEASE don't forget to add ", 'cf7-antispam' ) ); ?></h3>
-      <b><code class="blink"><?php echo esc_html( __( "flamingo_message: \"[your-message]\" ", 'cf7-antispam' ) ); ?></code></b>
-      <p><?php echo esc_html( __( "[your-message] or the name of your message field as you do with flamingo. This is very important otherwise the you can't get the full antispam functionality", 'cf7-antispam' ) ); ?></p>
-    </div></div>
+    <div class="card welcome-panel banner dismissible<?php echo $dismissible_banner_class ?>">
+			<div class="inside">
+				<a class="welcome-panel-close" href="<?php echo esc_url( add_query_arg( 'action', 'dismiss-banner', menu_page_url( 'cf7-antispam', false ) ) ); ?>"><?php echo esc_html( __( 'Dismiss', 'contact-form-7' ) ); ?></a>
+				<?php if ( !is_plugin_active( 'flamingo/flamingo.php' ) ) { ?>
+				<h3><span class="dashicons dashicons-editor-help" aria-hidden="true"></span> <?php echo esc_html( __( "Before you cry over spilt mail&#8230;", 'contact-form-7' ) ); ?></h3>
+				<p><?php echo esc_html( __( "Contact Form 7 doesn&#8217;t store submitted messages anywhere. Therefore, you may lose important messages forever if your mail server has issues or you make a mistake in mail configuration.", 'contact-form-7' ) ); ?></p>
+				<p><?php echo sprintf( /* translators: %s: link labeled 'Flamingo' */
+					esc_html( __( 'Install a message storage plugin before this happens to you. %s saves all messages through contact forms into the database. Flamingo is a free WordPress plugin created by the same author as Contact Form 7.', 'contact-form-7' ) ), wpcf7_link( __( 'https://contactform7.com/save-submitted-messages-with-flamingo/', 'contact-form-7' ), __( 'Flamingo', 'contact-form-7' ) ) );
+				?></p>
+				<hr />
+				<?php } ?>
+				<h3 class="blink"><span class="dashicons dashicons-megaphone" aria-hidden="true"></span> <?php echo esc_html( __( "PLEASE don't forget to add ", 'cf7-antispam' ) ); ?></h3>
+				<b><code class="blink"><?php echo esc_html( __( "flamingo_message: \"[your-message]\" ", 'cf7-antispam' ) ); ?></code></b>
+				<p>
+					<?php echo esc_html( __( "Please replace ", 'cf7-antispam' ) ); ?>
+					<b><?php echo	esc_attr( __('[your-message]', 'cf7-antispam' ) ); ?></b>
+					<?php echo esc_html( __( " with the message field used in your form because that is the field scanned with b8. You need add this string to each form ", 'cf7-antispam' ) ); ?>
+					<a href='https://contactform7.com/additional-settings/'><?php	echo esc_attr( __('additional settings section', 'cf7-antispam' ) ); ?></a>
+					<?php echo esc_html( __( "to enable the most advanced protection we can offer! Thank you!", 'cf7-antispam' ) ); ?>
+				</p>
+			</div>
+		</div>
 
     <div class="card main-options">
 
