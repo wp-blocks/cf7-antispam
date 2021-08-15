a19f1da7b52f279d374ef4e397e4fb1aaad7364c
Erik Yo <erikyo83@gmail.com>
2021-08-15 11:07:06 +0200

    plugin data will be not removed on deactivation (only on uninstall)

--- includes/cf7a-uninstall.php
+++ includes/cf7a-uninstall.php
@@ -8,21 +8,28 @@ class CF7_AntiSpam_Uninstaller {
 
 	public static function uninstall() {
 
-		if (CF7ANTISPAM_DEBUG) error_log(print_r(CF7ANTISPAM_LOG_PREFIX.'plugin uninstalled',true));
+		if (CF7ANTISPAM_DEBUG || CF7ANTISPAM_DEBUG_EXTENDED) {
 
-		global $wpdb;
+			global $wpdb;
 
-		$wpdb->query( "DROP TABLE IF EXISTS ". $wpdb->prefix ."cf7a_wordlist" );
-		$wpdb->query( "DROP TABLE IF EXISTS ". $wpdb->prefix ."cf7a_blacklist" );
+			$wpdb->query( "DROP TABLE IF EXISTS ". $wpdb->prefix ."cf7a_wordlist" );
+			$wpdb->query( "DROP TABLE IF EXISTS ". $wpdb->prefix ."cf7a_blacklist" );
 
-		delete_option("cf7a_db_version");
-		delete_option("cf7a_options");
+			delete_option("cf7a_db_version");
+			delete_option("cf7a_options");
 
-		delete_user_meta( get_current_user_id(), 'cf7a_hide_welcome_panel_on');
+			delete_user_meta( get_current_user_id(), 'cf7a_hide_welcome_panel_on');
 
-		require_once CF7ANTISPAM_PLUGIN_DIR . '/includes/cf7a-antispam.php';
+			require_once CF7ANTISPAM_PLUGIN_DIR . '/includes/cf7a-antispam.php';
+			CF7_AntiSpam_filters::cf7a_flamingo_on_uninstall();
 
-		CF7_AntiSpam_filters::cf7a_flamingo_on_uninstall();
+			error_log(print_r(CF7ANTISPAM_LOG_PREFIX.'plugin uninstalled',true));
+
+		} else {
+
+			error_log(print_r('CONTACT FORM 7 ANTISPAM - "CF7ANTISPAM_DEBUG_EXTENDED = true" so options and database will not be deleted.',true));
+
+		}
 
 	}
 
