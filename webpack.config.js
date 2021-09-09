945dad3d5c646812a8445c11cbc2320727a700f0
Erik Yo <erikyo83@gmail.com>
2021-09-09 02:31:04 +0200

    wp-script custom webpack babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
+require( '@wordpress/dependency-extraction-webpack-plugin' );
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
 const production = 'development' !== process.env.NODE_ENV;
@@ -8,7 +8,29 @@ module.exports = {
   module: {
     ...defaultConfig.module,
     rules: [
-      ...defaultConfig.module.rules
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					require.resolve( 'thread-loader' ),
+					{
+						loader: require.resolve('babel-loader'),
+						options: {
+							presets: [
+								["@babel/preset-env", {
+									useBuiltIns: "usage",
+									corejs: 3,
+								}],
+								'@wordpress/babel-preset-default'
+							],
+							plugins: [
+								'@babel/plugin-proposal-async-generator-functions',
+								'@babel/plugin-proposal-object-rest-spread'
+							]
+						},
+					}],
+			},
+			...defaultConfig.module.rules
     ]
   },
   plugins: [
