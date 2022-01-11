2eaa3ca6b0f3f3f9dde8e3ee5c67bcf2eb7f90f3
Erik Yo <erikyo83@gmail.com>
2022-01-11 14:37:01 +0100

    flamingo columns style

--- admin/css/style.asset.php
+++ admin/css/style.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'bcebd5a7f30683bd016801e24b014b99');
+<?php return array('dependencies' => array(), 'version' => 'c8df7e61b26efe65c8510c4defc66a78');
