8c26c0f9ede5b0ee3400004056cbf42507979bd0
Erik Golinelli <erik@codekraft.it>
2022-10-27 00:49:07 +0200

    tests init

---
+++ tests/test-CF7_AntiSpam_ActivatorTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+require_once '../cf7-antispam.php';
+
+class CF7_AntiSpam_ActivatorTest extends TestCase {
+
+
+	public function testActivate() {
+        return true;
+	}
+
+	public function testInstall() {
+		return true;
+	}
+
+	public function testUpdate_options() {
+		return true;
+	}
+}
