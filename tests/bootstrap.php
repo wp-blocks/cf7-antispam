9b9cf8ef20eef3360408a75d2928418cc3a3cd82
Erik Golinelli <erik@codekraft.it>
2022-10-27 15:55:42 +0200

    move composer autoload at the beginning of tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,14 @@
  * @package Cf7_Antispam
  */
 
+// Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available
+require_once dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php';
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = getenv( 'WP_PHPUNIT__DIR' );
+}
 
 if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
@@ -16,9 +23,6 @@ $_phpunit_polyfills_path = getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' );
 if ( false !== $_phpunit_polyfills_path ) {
 	define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', $_phpunit_polyfills_path );
 }
-
-// Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available
-require_once dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php';
 
 if ( ! file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
 	echo "Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
@@ -34,6 +38,8 @@ require_once "{$_tests_dir}/includes/functions.php";
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/cf7-antispam.php';
 }
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
 /**
  * If WordPress dies, throw an exception.
  *
@@ -49,7 +55,6 @@ function handle_wp_setup_failure( $message ) {
 }
 tests_add_filter( 'wp_die_handler', 'handle_wp_setup_failure' );
 
-
 /*
 * Load PHPUnit Polyfills for the WP testing suite.
 */
@@ -59,8 +64,6 @@ define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', __DIR__ . '/../../../vendor/yoast/php
  * Start up the WP testing environment.
  */
 require "{$_tests_dir}/includes/bootstrap.php";
-
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 remove_filter( 'wp_die_handler', 'handle_wp_setup_failure' );
 
