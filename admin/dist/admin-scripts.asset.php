1379b7579b9225c50947395527ef47c107295e43
Erik Golinelli <erik@codekraft.it>
2022-10-29 00:51:19 +0200

    updated dashboard chart script

---
+++ admin/dist/admin-scripts.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5eb2c54f19bac4571a98');
--- admin/src/dashboard/dashboard.js
+++ admin/src/dashboard/dashboard.js
@@ -1,53 +1,55 @@
 /* global spamChartData */
-import Chart from "chart.js/auto";
+import Chart from 'chart.js/auto';
 
 function spamCharts() {
-	const cf7aCharts = {};
+	if ( spamChartData ) {
+		const cf7aCharts = {};
 
-	const lineConfig = {
-		type: 'line',
-		data: spamChartData.lineData,
-		options: {
-			responsive: true,
-			plugins: {
-				legend: { display: false },
-			},
-			scales: {
-				y: {
-					ticks: {
-						min: 0,
-						precision: 0,
+		const lineConfig = {
+			type: 'line',
+			data: spamChartData.lineData,
+			options: {
+				responsive: true,
+				plugins: {
+					legend: { display: false },
+				},
+				scales: {
+					y: {
+						ticks: {
+							min: 0,
+							precision: 0,
+						},
 					},
 				},
 			},
-		},
-	};
+		};
 
-	const PieConfig = {
-		type: 'pie',
-		data: spamChartData.pieData,
-		options: {
-			responsive: true,
-			plugins: {
-				legend: { display: false },
+		const PieConfig = {
+			type: 'pie',
+			data: spamChartData.pieData,
+			options: {
+				responsive: true,
+				plugins: {
+					legend: { display: false },
+				},
 			},
-		},
-	};
+		};
 
-  console.log(spamChartData);
-  console.log(document.getElementById( 'line-chart' ));
+		console.log( spamChartData );
+		console.log( document.getElementById( 'line-chart' ) );
 
-	cf7aCharts.lineChart = new Chart(
-		document.getElementById( 'line-chart' ),
-		lineConfig
-	);
+		cf7aCharts.lineChart = new Chart(
+			document.getElementById( 'line-chart' ),
+			lineConfig
+		);
 
-	cf7aCharts.pieChart = new Chart(
-		document.getElementById( 'pie-chart' ),
-		PieConfig
-	);
+		cf7aCharts.pieChart = new Chart(
+			document.getElementById( 'pie-chart' ),
+			PieConfig
+		);
 
-	return cf7aCharts;
+		return cf7aCharts;
+	}
 }
 
 window.onload = spamCharts();
