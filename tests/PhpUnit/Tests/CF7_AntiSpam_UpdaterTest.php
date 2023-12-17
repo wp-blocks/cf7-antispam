104897aa20ec6262bf4852fc05f68ae7570033a0
gardenboi <dawidgrden.99@gmail.com>
2023-12-17 02:11:06 +0100

    wip, adding Updater class
    This class will be responsible for future database updates, further modifications are required

---
+++ tests/PhpUnit/Tests/CF7_AntiSpam_UpdaterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace CF7_AntiSpam\Tests\PhpUnit\Tests;
+
+use CF7_AntiSpam\Engine\CF7_AntiSpam_Updater;
+use PHPUnit\Framework\TestCase;
+
+class CF7_AntiSpam_UpdaterTest extends TestCase {
+
+	public function testUpdate_db_procedure_to_0_6_0() {
+
+		$v0_6_0 = '0.6.0';
+
+		$options_test_new = array(
+			'cf7a_version' => '0.6.0',
+			'languages_locales' => array(
+				'allowed' => array(
+					'languages' => array( 'en' ),
+					'locales' => array( 'US' )
+				),
+				'disallowed' => array(
+					'languages' => array( 'it' ),
+					'locales' => array( 'IT' )
+				)
+			)
+		);
+
+		$options_test_old = array(
+			'cf7a_version' => '0.4.5',
+			'languages' => array(
+				'allowed' => array( 'en', 'it-IT' ),
+				'disallowed' => array( 'fr', 'FR'),
+			)
+		);
+
+		$thisInstance = new CF7_AntiSpam_Updater($v0_6_0, $options_test_old['cf7a_version'], $options_test_old);
+		$result = $thisInstance->do_updates();
+		$this->assertEquals($options_test_new, $result ,
+			'error expected ' . print_r( $options_test_new, true ) .
+			" result " . print_r( $result, true ));
+	}
+
+
+}
