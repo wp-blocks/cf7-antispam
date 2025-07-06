af2f839fcabed0d74d4679d7f6923648c8a4485d
Erik Golinelli <erik@codekraft.it>
2025-07-06 12:50:08 +0200

    adding the tsConfig file and prepares the bundler to use the new ts file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-const path = require( 'path' );
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const path = require('path');
 
 module.exports = {
 	...defaultConfig,
 	entry: {
-		script: path.resolve( process.cwd(), `src/script.js` ),
-		'admin-scripts': path.resolve( process.cwd(), `src/admin-scripts.js` ),
+		script: path.resolve(process.cwd(), `src/script.ts`),
+		'admin-scripts': path.resolve(process.cwd(), `src/admin-scripts.js`),
 	},
 };
