6ea26a813895e643cba39da37a03ae793cfffa93
Erik Golinelli <erik@codekraft.it>
2025-10-24 14:54:32 +0200

    exportActionHandler

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -4,8 +4,54 @@ import { __ } from '@wordpress/i18n';
 window.addEventListener('load', adminSettingsHelper);
 
 /**
- * Resend message function
- * Displays a confirmation alert and sends a POST request to the API to resend a message.
+ * A function that handles all the function that download some files from the server via API
+ * @param e HTMLElement The button element
+ */
+function exportActionHandler(e: HTMLElement) {
+	const { action, nonce } = e.dataset as {
+		action: string;
+		nonce: string;
+	};
+
+	apiFetch({
+		path: '/cf7-antispam/v1/' + action,
+		method: 'POST',
+		data: {
+			nonce,
+		},
+	})
+		.then((res) => {
+			const { success, message, filetype, filename, data } = res as {
+				success: boolean;
+				message: string;
+				filetype: string;
+				filename: string;
+				data: string;
+			};
+			if (success) {
+				if (filetype === 'csv') {
+					const blob = new Blob([data], { type: 'text/csv' });
+					const url = window.URL.createObjectURL(blob);
+					const a = document.createElement('a');
+					a.style.display = 'none';
+					a.href = url;
+					a.download = filename;
+					document.body.appendChild(a);
+					a.click();
+					window.URL.revokeObjectURL(url);
+				}
+			} else {
+				console.error('Error: Failed to export file', message);
+			}
+		})
+		.catch((error: any) => {
+			console.error('Error:', error.message);
+			alert('Request failed: ' + error.message);
+		});
+}
+
+/**
+ * A function that handles the actions for the admin tabs
  *
  * @param e               HTMLElement The button element
  * @param dataset         object The dataset of the button element
@@ -104,10 +150,18 @@ function adminSettingsHelper() {
 		const actions = document.querySelectorAll(
 			'.cf7a_action'
 		) as NodeListOf<HTMLElement>;
+		const exportActions = document.querySelectorAll(
+			'.cf7a_export_action'
+		) as NodeListOf<HTMLElement>;
 
 		actions.forEach((action: HTMLElement) => {
 			action.addEventListener('click', () => {
 				actionHandler(action);
+			});
+		});
+		exportActions.forEach((action: HTMLElement) => {
+			action.addEventListener('click', () => {
+				exportActionHandler(action);
 			});
 		});
 	}
