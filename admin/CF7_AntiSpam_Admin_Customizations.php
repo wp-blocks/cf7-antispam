f3b0011dd264e43e84d36dafc5e725f3a8bc0eab
Erik Golinelli <erik@codekraft.it>
2025-11-01 11:25:28 +0100

    geoip database manual download endpoint

--- admin/CF7_AntiSpam_Admin_Customizations.php
+++ admin/CF7_AntiSpam_Admin_Customizations.php
@@ -1413,7 +1413,7 @@ class CF7_AntiSpam_Admin_Customizations {
 	/** Force database download button*/
 	public function cf7a_force_download_callback() {
 		// the upload button for the database if the download is disabled
-		printf( '<input type="button" id="geoip_force_download" class="cf7a_action" data-action="force-geoip-download" data-nonce="%s" value="%s" />',
+		printf( '<input type="button" id="geoip_force_download" class="button cf7a_action" data-action="force-geoip-download" data-nonce="%s" value="%s" />',
 			wp_create_nonce( 'cf7a-nonce' ),
 			esc_attr__( 'Force Download', 'cf7-antispam' )
 		);
